0e4d4885ff88721d122070506ea6941579f07ea9
Sayantansen123 <riosayan123@gmail.com>
2025-07-24 15:34:48 +0530

    database succesfully connected

--- server/index.mjs
+++ server/index.mjs
@@ -1,16 +1,22 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import connectDB from "./db/connect.js";
+
+//graphql type definitions
 const typeDefs = `#graphql
   type Query {
     hello: String
   }
 `;
 
+//resolver to give what users want
 const resolvers = {
   Query: {
     hello: () => "Hello from Apollo Server v5!",
   },
 };
+
+await connectDB();
 
 const server = new ApolloServer({
   typeDefs,
