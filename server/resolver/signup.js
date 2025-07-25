e0274ce5fb34e0e01a6aa00683ed81d5dda4e63a
Sayantansen123 <riosayan123@gmail.com>
2025-07-25 23:48:28 +0530

    updated

--- server/resolver/signup.js
+++ server/resolver/signup.js
@@ -7,16 +7,16 @@ const SECRET_KEY = "secretkey";
 export const signupResolver = async (_, { input }) => {
   const { name, email, password, work } = input;
 
-  // 1. Check if user already exists
+  // Check if user already exists
   const existingUser = await User.findOne({ email });
   if (existingUser) {
     throw new Error("User already exists");
   }
 
-  // 2. Hash the password
+  // Hash the password
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // 3. Save the new user
+  // Save the new user
   const user = new User({
     name,
     email,
@@ -26,10 +26,10 @@ export const signupResolver = async (_, { input }) => {
 
   await user.save();
 
-  // 4. Generate JWT token
+  // Generate JWT token
   const token = jwt.sign({ userId: user._id }, SECRET_KEY, { expiresIn: "7d" });
 
-  // 5. Return auth payload
+  // Return auth payload
   return {
     token,
     user,
